dfaa61135061af4922479b21da0f787f8891caeb
Persephone <persefona212@gmail.com>
2023-04-12 22:14:56 +0600

    Задача10

---
+++ HW_09.04.23/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число
+// и на выходе показывает вторую цифру этого числа.
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+Console.Write("Введите трёхзначное число: ");
+int number = int.Parse(Console.ReadLine());
+int second = 0;
+
+if (number > 99 && number < 1000)
+{
+    second = (number / 10) % 10;
+    Console.WriteLine("Вторая цифра: " + second);
+}
+else
+{
+    Console.WriteLine("Введены некорректные данные");
+}
