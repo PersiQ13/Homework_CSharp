bdb0ec9b00ca291cd5c5ce6322ea3e96bbcc39c5
Persephone <persefona212@gmail.com>
2023-04-22 19:33:25 +0600

    Задача_34

---
+++ HW_22.04_34/Program.cs
@@ -0,0 +1,25 @@
+﻿/* Задайте массив заполненный случайными положительными трёхзначными числами. 
+Напишите программу, которая покажет количество чётных чисел в массиве. */
+
+int [] array = new int[10];
+Random rand = new Random();
+
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = rand.Next(100, 999);
+}
+
+int count = 0;
+
+Console.WriteLine("Массив случайных трехзначных чисел: ");
+
+for (int i = 0; i < 10; i++)
+{
+    Console.Write(array[i] + " ");
+    if (array[i] % 2 == 0)
+        {
+            count++; 
+        }
+}
+Console.WriteLine();
+Console.WriteLine("Количество четных чисел в массиве: " + count);
