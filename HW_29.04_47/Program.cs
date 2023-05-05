c8dc004c0897eb34de6e008d2414acb509751003
Persephone <persefona212@gmail.com>
2023-05-05 20:56:42 +0600

    Задача_47

---
+++ HW_29.04_47/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+
+int m = 3;
+int n = 4;
+
+double[,] arr = new double[m, n];
+
+Random rnd = new Random();
+
+for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            arr[i, j] = rnd.NextDouble() * 10;
+            Console.Write("{0:F2} ", arr[i, j]);
+        }
+        Console.WriteLine();
+    }
