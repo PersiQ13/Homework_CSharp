d315ef525ac9d804fd702adf16859884a6b4480d
Persephone <persefona212@gmail.com>
2023-05-27 20:37:41 +0600

    Задача_64

---
+++ HW_21.05_64/Program.cs
@@ -0,0 +1,17 @@
+﻿/* Задайте значение N. Напишите программу, 
+которая выведет все натуральные числа в промежутке от N до 1. 
+Выполнить с помощью рекурсии. 
+N = 5; N = 8 */
+
+int n = 8;
+PrintNumbers(n);
+
+static void PrintNumbers(int n)
+{
+    Console.Write(n + " ");
+
+    if (n == 1) 
+        return;
+
+    PrintNumbers(n - 1);
+}
